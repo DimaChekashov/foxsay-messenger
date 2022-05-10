c70b261c7e58d1a67ae7957e575714d4bab4c4c4
DimaChekashov <foxdima99@gmail.com>
2022-05-10 04:30:04 +0300

    connect to firebase

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Context } from '../../index';
+import firebase from 'firebase';
 import './Login.scss';
 
 const Login: React.FC = () => {
+  const { auth } = useContext(Context);
+
+  const login = async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    const { user } = await auth.signInWithPopup(provider);
+    console.log(user);
+  }
+
   return (
     <div className="login">
-      <button className="login__btn">Login via Google</button>
+      <button className="login__btn" onClick={login}>Login via Google</button>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import firebase from "firebase";
+import 'firebase/firestore';
+import 'firebase/auth';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCYfMNDRm80j7Javt2TeHoaDi1pKLCE9Tg",
+  authDomain: "foxsay-messenger.firebaseapp.com",
+  projectId: "foxsay-messenger",
+  storageBucket: "foxsay-messenger.appspot.com",
+  messagingSenderId: "963280914704",
+  appId: "1:963280914704:web:7e0c06bc27fd9b8d807d95",
+  measurementId: "G-2LSRKEXZM5"
+});
+
+export const Context = createContext<any>(null);
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <Context.Provider value={{
+    firebase,
+    auth,
+    firestore
+  }}>
     <App />
-  </React.StrictMode>
+  </Context.Provider>
 );
