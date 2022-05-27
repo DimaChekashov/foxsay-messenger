f38ec17822b950b3479404d219da7de57d4aacd5
DimaChekashov <foxdima99@gmail.com>
2022-05-27 23:11:53 +0300

    add navbar Info

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,24 +1,32 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ROUTE } from '../../utils/consts';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Context } from '../..';
+import cn from "classnames";
 import './Navbar.scss';
 
 const Navbar: React.FC = () => {
     const { auth } = useContext(Context);
     const [ user ] = useAuthState(auth);
+    const [navbarMenu, setNavbarMenu] = useState<boolean>(false);
 
     return (
         <div className="navbar">
             {user && (
-                <div className="navbar__profile">
-                    <div className="navbar__profile-avatar">
+                <div className={cn("navbar__profile", navbarMenu ? "active" : "")}>
+                    <div className="navbar__profile-avatar" onClick={() => setNavbarMenu(!navbarMenu)}>
                         <img src={user.photoURL} alt="profile avatar" />
                     </div>
                     <div>
                         <div className="navbar__profile-username">{user.displayName}</div>
                         <div className="navbar__profile-email">{user.email}</div>
+                    </div>
+                    <div className="navbar__profile-menu">
+                        <div className="navbar__profile-createAt">
+                            <span>Сreated at:</span>
+                            {user.metadata.creationTime}
+                        </div>
                     </div>
                 </div>
             )}
